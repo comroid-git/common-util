3e1a0d8d1de9af49bd01b553e315a775295cf9ab
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2020-02-25 17:21:13 +0100

    Release stuff

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.comroid.common.spellbind"},{"l":"org.comroid.common.spellbind.model"},{"l":"org.comroid.common.trie"}]
