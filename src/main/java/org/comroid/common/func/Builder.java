6a2c9096daa1770a7dd77c4f83a32a44835ea28a
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2020-02-25 23:52:40 +0100

    introduced main module

---
+++ src/main/java/org/comroid/common/func/Builder.java
@@ -0,0 +1,5 @@
+package org.comroid.common.func;
+
+public interface Builder<T> {
+    T build();
+}
---
+++ src/main/java/org/comroid/common/func/Factory.java
@@ -0,0 +1,7 @@
+package org.comroid.common.func;
+
+public interface Factory<T> {
+    int counter();
+    
+    T create();
+}
