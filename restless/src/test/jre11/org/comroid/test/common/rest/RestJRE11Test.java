4cb1407c637265ec590de42c5993fee6674cac40
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2020-02-29 15:26:26 +0100

    gradle enhancements

---
+++ restless/src/test/jre11/org/comroid/test/common/rest/RestJRE11Test.java
@@ -0,0 +1,9 @@
+package org.comroid.test.common.rest;
+
+import org.junit.Test;
+
+public class RestJRE11Test {
+    @Test
+    public void testClient() {
+    }
+}
---
+++ restless/src/test/jre8/org/comroid/test/common/rest/RestJRE8Test.java
@@ -0,0 +1,9 @@
+package org.comroid.test.common.rest;
+
+import org.junit.Test;
+
+public class RestJRE8Test {
+    @Test
+    public void testClient() {
+    }
+}
