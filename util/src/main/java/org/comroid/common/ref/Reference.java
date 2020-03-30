2bb45a247a472fb9ee0940192561034f14dcef09
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-03-30 21:05:29 +0200

    Create Reference.java

---
+++ util/src/main/java/org/comroid/common/ref/Reference.java
@@ -0,0 +1,25 @@
+package org.comroid.common.ref;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public interface Reference<T> extends Supplier<T> {
+    @Override
+    @Nullable T get();
+
+    default Optional<T> wrap() {
+        return Optional.ofNullable(get());
+    }
+
+    default @NotNull T requireNonNull() throws NullPointerException {
+        return Objects.requireNonNull(get());
+    }
+
+    default @NotNull T requireNonNull(String message) throws NullPointerException {
+        return Objects.requireNonNull(get(), message);
+    }
+}
