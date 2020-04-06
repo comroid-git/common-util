2228064e08d2914f8e740b75ab1328cc021be35b
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2020-04-06 18:17:55 +0200

    remove main function

--- util/src/main/java/org/comroid/common/util/ArrayUtil.java
+++ util/src/main/java/org/comroid/common/util/ArrayUtil.java
@@ -3,15 +3,6 @@ package org.comroid.common.util;
 import java.util.Arrays;
 
 public final class ArrayUtil {
-    public static void main(String[] args) {
-        Integer[] x = new Integer[]{0,1,2,3};
-        Integer[] y = new Integer[]{9,8,7};
-
-        Integer[] yield = ArrayUtil.insert(x, 2, y);
-
-        System.out.println("yield = " + Arrays.toString(yield));
-    }
-
     @SafeVarargs
     public static <T> T[] insert(T[] original, int atIndex, T... insert) {
         final T[] yield = Arrays.copyOf(original, original.length + insert.length);
