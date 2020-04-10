a1d804cdcb317a39e763a08bbf9bb0d6ff99a39a
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-04-10 17:59:20 +0200

    Update OptionalVararg.java

--- util/src/main/java/org/comroid/common/annotation/OptionalVararg.java
+++ util/src/main/java/org/comroid/common/annotation/OptionalVararg.java
@@ -5,11 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Marks a VarArg parameter as an optional parameter, expecting this behavior: - The Parameter can
- * be left out -> Expects the method to provide a default value - The Parameter can be defined with
- * 1 element - The Method accepts a VarArgs array with up to {@link #limit()} elements
- */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.PARAMETER)
 public @interface OptionalVararg {
