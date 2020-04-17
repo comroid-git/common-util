a51018b6ceb0822c54c0e815d5a3e45e53314d76
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2020-04-18 00:43:55 +0200

    Create MultipleExceptions.java

---
+++ util/src/main/java/org/comroid/common/exception/MultipleExceptions.java
@@ -0,0 +1,44 @@
+package org.comroid.common.exception;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Collection;
+
+public class MultipleExceptions extends RuntimeException {
+    public MultipleExceptions(String message, Collection<Throwable> causes) {
+        super(composeMessage(message, causes));
+    }
+
+    public MultipleExceptions(Collection<Throwable> causes) {
+        super(composeMessage(null, causes));
+    }
+
+    private static String composeMessage(@Nullable String baseMessage, Collection<Throwable> throwables) {
+        class StringStream extends OutputStream {
+            private final StringBuilder sb = new StringBuilder();
+
+            @Override
+            public void write(int b) {
+                sb.append((char) b);
+            }
+
+            @Override
+            public String toString() {
+                return sb.toString();
+            }
+        }
+
+        if (baseMessage == null)
+            baseMessage = "Multiple Exceptions were thrown";
+        final StringStream out    = new StringStream();
+        final PrintStream  string = new PrintStream(out);
+
+        string.println(baseMessage);
+        string.println("Sub Stacktraces in order:");
+        throwables.forEach(t -> t.printStackTrace(string));
+
+        return out.toString();
+    }
+}
