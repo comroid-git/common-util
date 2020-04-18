2d3791f3723ad5ccd8152d8dff78469cf17e45fb
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2020-04-18 18:16:26 +0200

    use future completion on new references

--- uniform/src/main/java/org/comroid/uniform/cache/Cache.java
+++ uniform/src/main/java/org/comroid/uniform/cache/Cache.java
@@ -1,6 +1,8 @@
 package org.comroid.uniform.cache;
 
 import org.comroid.common.Polyfill;
+import org.comroid.common.func.Provider;
+import org.comroid.common.ref.OutdateableReference;
 import org.comroid.common.ref.Reference.Settable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -10,7 +12,6 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -79,12 +80,19 @@ public interface Cache<K, V> extends Iterable<Map.Entry<K, V>> {
             }
         };
 
-        public final AtomicReference<V> reference = new AtomicReference<>(null);
-        private final Object             lock      = Polyfill.selfawareLock();
+        public final AtomicReference<V>                          reference        = new AtomicReference<>(null);
+        private final OutdateableReference<CompletableFuture<V>> firstValueFuture = new OutdateableReference<>();
+        private final Object                                     lock             = Polyfill.selfawareLock();
         private final K                  key;
 
         public Reference(K key) {
             this.key = key;
+            this.firstValueFuture.update(new CompletableFuture<>());
+        }
+
+        public Reference(K key, V initValue) {
+            this.key = key;
+            this.firstValueFuture.outdate();
         }
 
         public @NotNull K getKey() {
@@ -95,6 +103,9 @@ public interface Cache<K, V> extends Iterable<Map.Entry<K, V>> {
         @Override
         public V set(V value) {
             synchronized (lock) {
+                if (!firstValueFuture.isOutdated() && !firstValueFuture.get().isDone())
+                    firstValueFuture.get().complete(value);
+
                 return reference.getAndSet(value);
             }
         }
@@ -105,6 +116,16 @@ public interface Cache<K, V> extends Iterable<Map.Entry<K, V>> {
             synchronized (lock) {
                 return reference.get();
             }
+        }
+
+        @Override
+        public Provider<V> provider() {
+            if (firstValueFuture.isOutdated()) {
+                firstValueFuture.outdate();
+                return Provider.of(firstValueFuture.get());
+            }
+
+            return Provider.of(this);
         }
 
         public static <K, V> Reference<K, V> empty() {
