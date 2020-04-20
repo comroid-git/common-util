c6dc1a7399c3c68613e40e66855c85aa2c739f73
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2020-04-20 19:37:11 +0200

    update invocable interface

--- spellbind/src/main/java/org/comroid/common/spellbind/factory/InstanceFactory.java
+++ spellbind/src/main/java/org/comroid/common/spellbind/factory/InstanceFactory.java
@@ -1,26 +1,109 @@
 package org.comroid.common.spellbind.factory;
 
 import org.comroid.common.func.ParamFactory;
-import org.comroid.common.map.TrieMap;
+import org.comroid.common.map.TrieFuncMap;
+import org.comroid.common.ref.Pair;
 import org.comroid.common.spellbind.model.Invocable;
-import org.comroid.common.util.ReflectionHelper;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
-public final class InstanceFactory<C extends InstanceContext<C>, T> extends ParamFactory.Abstract<C, T> {
-    private final Map<String, Invocable> strategies = TrieMap.ofString();
+public final class InstanceFactory<T, C extends InstanceContext<C>> extends ParamFactory.Abstract<C, T> {
+    private final Map<Class[], Invocable> strategies;
+
+    private InstanceFactory(Map<Class[], Invocable> strategies) {
+        this.strategies = strategies;
+    }
 
     @Override
     public T create(@Nullable C parameter) {
-        final String argsFootprint = Stream.of(parameter.getArgs())
+        final Class[] types = Arrays.stream(parameter.getArgs())
                 .map(Object::getClass)
-                .map(ReflectionHelper::canonicalClass)
-                .map(Class::getCanonicalName)
-                .collect(Collectors.joining(", ", "(", ")"));
+                .toArray(Class[]::new);
+        final Invocable invocable = strategies.get(types);
 
-        return null;
+        if (invocable == null)
+            throw new UnsupportedOperationException(String.format("Cannot construct from types %s",
+                    Arrays.toString(types)
+            ));
+
+        try {
+            return (T) invocable.invoke(parameter.getArgs());
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final class Builder<T, C extends InstanceContext<C>>
+            implements org.comroid.common.func.Builder<InstanceFactory<T, C>> {
+        private ParamFactory<Object[], Object> coreObjectFactory;
+        private List<ImplementationNotation>   implementations = new ArrayList<>();
+        private ClassLoader                    classLoader;
+
+        public Builder(Class<T> mainInterface) {
+        }
+
+        public Builder<T, C> coreObject(ParamFactory<Object[], Object> coreObjectFactory) {
+            this.coreObjectFactory = coreObjectFactory;
+        }
+
+        public Builder<T, C> subImplement(ParamFactory<Object[], Object> subFactory, Class<?> asInterface) {
+            this.implementations.add(new ImplementationNotation(subFactory, asInterface));
+        }
+
+        public Builder<T, C> classloader(ClassLoader classLoader) {
+            this.classLoader = classLoader;
+        }
+
+        @Override
+        public InstanceFactory<T, C> build() {
+            final Map<Class[], Invocable> strategies
+                    = new TrieFuncMap<>((BiFunction<Class, Class, Boolean>) Class::isAssignableFrom,
+                    Function.identity()
+            );
+
+            return null;
+        }
+    }
+
+    private static final class OrderedParamInstanceFactory<T> extends ParamFactory.Abstract<Object[], T> {
+        private final Invocable invocable;
+
+        private OrderedParamInstanceFactory(Invocable invocable) {
+            this.invocable = invocable;
+        }
+
+        public Class[] getTypeOrder() {
+            return invocable.typeOrder();
+        }
+
+        @Override
+        public T create(@Nullable Object[] parameter) {
+            try {
+                return (T) invocable.invoke(parameter);
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static final class ImplementationNotation extends Pair<ParamFactory<Object[], Object>, Class<?>> {
+        public ImplementationNotation(ParamFactory<Object[], Object> first, Class<?> second) {
+            super(first, second);
+        }
+
+        public ParamFactory<Object[], Object> getFactory() {
+            return super.getFirst();
+        }
+
+        public Class<?> getType() {
+            return super.getSecond();
+        }
     }
 }
--- spellbind/src/main/java/org/comroid/common/spellbind/model/Invocable.java
+++ spellbind/src/main/java/org/comroid/common/spellbind/model/Invocable.java
@@ -1,10 +1,77 @@
 package org.comroid.common.spellbind.model;
 
-import java.lang.reflect.InvocationTargetException;
-
 import org.jetbrains.annotations.Nullable;
 
-public interface Invocable {
-    @Nullable Object invoke(Object... args)
-            throws InvocationTargetException, IllegalAccessException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public interface Invocable<T> {
+    @Nullable T invoke(Object... args) throws InvocationTargetException, IllegalAccessException;
+
+    Class[] typeOrder();
+
+    static <T> Invocable<T> ofConstructor(Constructor<T> constructor) {
+        return new Support.OfConstructor<>(constructor);
+    }
+
+    static <T> Invocable<T> ofMethodCall(@Nullable Method method) {
+        return new Support.OfMethod<>(method, null);
+    }
+
+    static <T> Invocable<T> ofMethodCall(Method method, @Nullable Object target) {
+        return new Support.OfMethod<>(method, target);
+    }
+
+    final class Support {
+        private static final class OfConstructor<T> implements Invocable<T> {
+            private final Constructor<T> constructor;
+
+            public OfConstructor(Constructor<T> constructor) {
+                this.constructor = constructor;
+            }
+
+            @Override
+            public @Nullable T invoke(Object... args) throws InvocationTargetException, IllegalAccessException {
+                try {
+                    return constructor.newInstance(args);
+                } catch (InstantiationException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public Class[] typeOrder() {
+                return constructor.getParameterTypes();
+            }
+        }
+
+        private static final class OfMethod<T> implements Invocable<T> {
+            private final Method method;
+            private final Object target;
+
+            private OfMethod(Method method, @Nullable Object target) {
+                if (target == null && !Modifier.isStatic(method.getModifiers()))
+                    throw new IllegalArgumentException(
+                            "Target cannot be null on non-static methods!",
+                            new NullPointerException()
+                    );
+
+                this.method = method;
+                this.target = target;
+            }
+
+            @Nullable
+            @Override
+            public T invoke(Object... args) throws InvocationTargetException, IllegalAccessException {
+                return (T) method.invoke(target, args);
+            }
+
+            @Override
+            public Class[] typeOrder() {
+                return method.getParameterTypes();
+            }
+        }
+    }
 }
