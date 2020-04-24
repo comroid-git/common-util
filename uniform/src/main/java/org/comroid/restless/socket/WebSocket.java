b5e88502c6a34bdc07d6b46df9e8706fb91a1160
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-04-24 17:13:27 +0200

    websocket fix

--- uniform/src/main/java/org/comroid/restless/socket/WebSocket.java
+++ uniform/src/main/java/org/comroid/restless/socket/WebSocket.java
@@ -1,15 +1,33 @@
 package org.comroid.restless.socket;
 
+import java.util.ArrayList;
+
 import org.comroid.dreadpool.ThreadPool;
 import org.comroid.listnr.EventHub;
 import org.comroid.uniform.node.UniObjectNode;
 
 public abstract class WebSocket {
+    public static final class Header {
+        public static final class List extends ArrayList<Header> {
+            public boolean add(String name, String value) {
+                return super.add(new Header(name, value));
+            }
+        }
+
+        private final String name;
+        private final String value;
+
+        public Header(String name, String value) {
+            this.name  = name;
+            this.value = value;
+        }
+    }
+
     private final EventHub<UniObjectNode> eventHub;
-    private final SocketEvent.Container eventContainer;
+    private final SocketEvent.Container   eventContainer;
 
     protected WebSocket(ThreadGroup threadGroup) {
-        this.eventHub = new EventHub<>(ThreadPool.fixedSize(threadGroup, 4));
-        this.eventContainer = new SocketEvent.Container(this.eventHub);
+        this.eventHub       = new EventHub<>(ThreadPool.fixedSize(threadGroup, 4));
+        this.eventContainer = new SocketEvent.Container(this, eventHub);
     }
 }
