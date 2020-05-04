b32515c4da783b578fccf9ff084a3e79e7c80135
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-05-04 19:52:21 +0200

    file cache now

--- uniform/src/main/java/org/comroid/restless/RestEndpoint.java
+++ uniform/src/main/java/org/comroid/restless/RestEndpoint.java
@@ -1,26 +1,77 @@
 package org.comroid.restless;
 
-import java.net.URL;
-import java.util.regex.Pattern;
-
 import org.comroid.common.Polyfill;
-import org.comroid.common.func.StringPattern;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.function.Function;
+import java.util.function.IntUnaryOperator;
+import java.util.regex.Pattern;
 
 public interface RestEndpoint {
     String getUrlBase();
 
     String getUrlExtension();
 
-    StringPattern getPattern();
+    Pattern getPattern();
+
+    IntUnaryOperator getGroupFx();
 
     default int getParameterCount() {
         return getUrlExtension().split("%s").length - 1;
     }
 
-    default URL create(Object... args) throws IllegalArgumentException {
-        if (args.length != getParameterCount())
-            throw new IllegalArgumentException("Invalid argument count");
+    default String getFullUrl() {
+        return getUrlBase() + getUrlExtension();
+    }
 
-        return Polyfill.url(String.format(getUrlBase() + getUrlExtension(), args));
+    default URL url(Object... args) throws IllegalArgumentException {
+        return makeAndValidateUrl(Polyfill::url, args);
+    }
+
+    default URI uri(Object... args) throws IllegalArgumentException {
+        return makeAndValidateUrl(Polyfill::uri, args);
+    }
+
+    default boolean testURL(URL url) {
+        return getPattern().matcher(url.toExternalForm())
+                .replaceAll(processEndpointUrl(getGroupFx()))
+                .equals(url.toExternalForm());
+    }
+
+    default boolean testURI(URI uri) {
+        return getPattern().matcher(uri.toString())
+                .replaceAll(processEndpointUrl(getGroupFx()))
+                .equals(uri.toString());
+    }
+
+    default String processEndpointUrl(IntUnaryOperator groupFx) {
+        String yield = getFullUrl();
+        int c = groupFx.applyAsInt(-1);
+
+        while (yield.contains("%s")) {
+            if (c == -1) {
+                return yield;
+            }
+
+            int fi = yield.indexOf("%s");
+            yield = String.format("%s$%d%s", yield.substring(0, fi), c, yield.substring(fi + 2));
+            c = groupFx.applyAsInt(c);
+        }
+
+        return yield;
+    }
+
+    default <T> T makeAndValidateUrl(Function<String, T> maker, Object... args) {
+        if (args.length != getParameterCount()) {
+            throw new IllegalArgumentException("Invalid argument count");
+        }
+
+        T url = maker.apply(String.format(getFullUrl(), args));
+        if ((url instanceof URL && testURL((URL) url)) || (url instanceof URI && testURI((URI) url))) {
+            return url;
+        }
+
+        throw new IllegalArgumentException("Generated URL is invalid");
     }
 }
