d9293a7aca972e3628b1e6b3f486d77e4b7bf800
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-05-30 17:14:57 +0200

    Added Java properties adapter

---
+++ uniform/src/main/jdk8/org/comroid/uniform/adapter/properties/jdk/JavaPropertiesSerializationAdapter.java
@@ -0,0 +1,43 @@
+package org.comroid.uniform.adapter.properties.jdk;
+
+import org.comroid.uniform.SerializationAdapter;
+import org.comroid.uniform.node.UniArrayNode;
+import org.comroid.uniform.node.UniNode;
+import org.comroid.uniform.node.UniObjectNode;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Properties;
+
+public class JavaPropertiesSerializationAdapter extends SerializationAdapter<Properties, Properties, Properties> {
+    protected JavaPropertiesSerializationAdapter() {
+        super("text/x-java-propertie", Properties.class, Properties.class);
+    }
+
+    @Override
+    public UniNode parse(@Nullable String data) {
+        return createUniObjectNode(ofString(data));
+    }
+
+    @Override
+    public UniObjectNode createUniObjectNode(Properties node) {
+        return new UniObjectNode(this, new PropertiesAdapter(node));
+    }
+
+    @Override
+    public UniArrayNode createUniArrayNode(Properties node) {
+        throw new UnsupportedOperationException("Cannot create ArrayNode for Properties");
+    }
+
+    private Properties ofString(String data) {
+        try {
+            final Properties prop = new Properties();
+            prop.load(new StringReader(data));
+
+            return prop;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
