bde7d4783b164c4786478b6f19c1f7a4035b58a4
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-06-05 17:06:15 +0200

    cleanup

--- src/uniform/main/java/org/comroid/restless/server/EndpointHandler.java
+++
@@ -1,8 +0,0 @@
-package org.comroid.restless.server;
-
-import org.comroid.restless.REST;
-import org.comroid.uniform.node.UniNode;
-
-public interface EndpointHandler {
-    REST.Response handle(UniNode body, String[] urlArgs);
-}
