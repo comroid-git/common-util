618d78a4ccf017b2cbe7e0f3409d73b93790ff59
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-06-14 14:46:58 +0200

    Improved TrieMap Test

--- src/trie/test/java/org/comroid/test/trie/TrieMapTest.java
+++ src/trie/test/java/org/comroid/test/trie/TrieMapTest.java
@@ -6,10 +6,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.lang.System.nanoTime;
+
 public class TrieMapTest {
+    public static final int TEST_SIZE = 5000;
     private Set<UUID> ids;
     private TrieMap<String, UUID> trie;
 
@@ -17,7 +21,7 @@ public class TrieMapTest {
     public void setup() {
         trie = TrieMap.ofString();
 
-        Map<String, List<UUID>> map = IntStream.range(0, 50)
+        Map<String, List<UUID>> map = IntStream.range(0, TEST_SIZE)
                 .mapToObj(x -> UUID.randomUUID())
                 .collect(Collectors.groupingBy(UUID::toString));
 
@@ -30,7 +34,29 @@ public class TrieMapTest {
 
     @Test
     public void testBasic() {
-        Assert.assertEquals(50, trie.size());
+        assertions();
+    }
+
+    @Test
+    public void testPerformance() {
+        System.out.printf("Starting TrieMap performance test at %d nanoTime\n", nanoTime());
+
+        IntStream.range(0, TEST_SIZE / 50)
+                .sequential()
+                .mapToLong(x -> nanoTime())
+                .peek(x -> assertions())
+                .map(x -> nanoTime() - x)
+                .map(TimeUnit.NANOSECONDS::toMillis)
+                .mapToObj(x -> {
+                    if (x > 200)
+                        return String.format("Assertions for %d IDs took a long time; finished in %d ms", TEST_SIZE, x);
+                    return String.format("Assertions for %d IDs finished in %d ms", TEST_SIZE, x);
+                })
+                .forEachOrdered(System.out::println);
+    }
+
+    private void assertions() {
+        Assert.assertEquals(TEST_SIZE, trie.size());
 
         ids.forEach(uuid -> {
             final String str = uuid.toString();
