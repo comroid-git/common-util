0238439db7d821b9bd16f51f337c5ab79b906747
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-06-19 21:29:55 +0200

    expanded BiPipeTest

--- src/mutatio/test/java/org/comroid/test/mutatio/pipe/BiPipeTest.java
+++ src/mutatio/test/java/org/comroid/test/mutatio/pipe/BiPipeTest.java
@@ -27,4 +27,21 @@ public class BiPipeTest {
                 .bi(String::length)
                 .forEach((str, len) -> Assert.assertEquals(str.length(), (int) len));
     }
+
+    @Test
+    public void testRemapFirst() {
+        refs.pipe()
+                .bi(String::length)
+                .filter((str, len) -> len > 3)
+                .mapFirst(str -> str.substring(str.length() + 2))
+                .forEach((str, len) -> Assert.assertEquals((int) len, str.length() - 2));
+    }
+
+    @Test
+    public void testRemapSecond() {
+        refs.pipe()
+                .bi(String::length)
+                .mapSecond(x -> x * 2)
+                .forEach((str, len) -> Assert.assertEquals(str.length() * 2, (int) len));
+    }
 }
