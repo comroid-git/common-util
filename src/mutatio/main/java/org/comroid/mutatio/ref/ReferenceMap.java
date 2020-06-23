5923bb9a4ce3e4787f228bc04a3edd1e596f3baf
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-06-23 18:38:34 +0200

    Added pipe methods to ReferenceMap

--- src/mutatio/main/java/org/comroid/mutatio/ref/ReferenceMap.java
+++ src/mutatio/main/java/org/comroid/mutatio/ref/ReferenceMap.java
@@ -1,18 +1,27 @@
 package org.comroid.mutatio.ref;
 
+import org.comroid.mutatio.pipe.BiPipe;
+import org.comroid.mutatio.pipe.Pipe;
+import org.comroid.mutatio.pipe.Pipeable;
+import org.comroid.mutatio.pump.Pump;
+import org.comroid.util.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.Executor;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public interface ReferenceMap<K, V, REF extends Reference<V>> {
+public interface ReferenceMap<K, V, REF extends Reference<V>> extends Pipeable<V> {
     default REF getReference(K key) {
         return getReference(key, false);
     }
 
     REF getReference(K key, boolean createIfAbsent);
+
+    ReferenceIndex<Map.Entry<K, V>> entryIndex();
 
     default V get(K key) {
         return getReference(key).get();
@@ -28,6 +37,25 @@ public interface ReferenceMap<K, V, REF extends Reference<V>> {
 
     default @NotNull V requireNonNull(K key, String message) {
         return getReference(key).requireNonNull(message);
+    }
+
+    @Override
+    default Pipe<?, V> pipe() {
+        return entryIndex()
+                .pipe()
+                .map(Map.Entry::getValue);
+    }
+
+    default BiPipe<?, ?, K, V> biPipe() {
+        return entryIndex()
+                .pipe()
+                .bi(Map.Entry::getValue)
+                .mapFirst(Map.Entry::getKey);
+    }
+
+    @Override
+    default Pump<?, V> pump(Executor executor) {
+        return pipe().pump(executor);
     }
 
     interface Settable<K, V, REF extends Reference.Settable<V>> extends ReferenceMap<K, V, REF> {
