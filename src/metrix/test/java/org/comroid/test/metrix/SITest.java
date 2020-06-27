9dfdf898ba5a569712fb26fbc367d0ef72d2f09d
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-06-27 14:04:14 +0200

    Expanded test output

--- src/metrix/test/java/org/comroid/test/metrix/SITest.java
+++ src/metrix/test/java/org/comroid/test/metrix/SITest.java
@@ -15,12 +15,28 @@ public class SITest {
                 .bi(si -> Meter.of(5, si))
                 .peek((si, len) -> System.out.printf("Testing unit %s with %s\n", si, len))
                 .forEach((si, meter) -> {
-                    si.above().map(unit -> si.convertTo(unit, 5))
-                            .ifPresent(expected -> Assert.assertEquals(expected, meter
-                                    .getAs(si.above().orElseThrow(AssertionError::new)), 0));
-                    si.below().map(unit -> si.convertTo(unit, 5))
-                            .ifPresent(expected -> Assert.assertEquals(expected, meter
-                                    .getAs(si.below().orElseThrow(AssertionError::new)), 0));
+                    si.above()
+                            .map(unit -> {
+                                System.out.printf("Converting %s -> %s...\n", si, unit);
+                                return si.convertTo(unit, 5);
+                            })
+                            .ifPresent(expected -> Assert.assertEquals(
+                                    String.format("Converting %s -> %s", si, si.above().orElse(null)),
+                                    expected,
+                                    meter.getAs(si.above().orElseThrow(AssertionError::new)),
+                                    0
+                            ));
+                    si.below()
+                            .map(unit -> {
+                                System.out.printf("Converting %s -> %s...\n", si, unit);
+                                return si.convertTo(unit, 5);
+                            })
+                            .ifPresent(expected -> Assert.assertEquals(
+                                    String.format("Converting %s -> %s", si, si.below().orElse(null)),
+                                    expected,
+                                    meter.getAs(si.below().orElseThrow(AssertionError::new)),
+                                    0
+                            ));
                 });
     }
 }
