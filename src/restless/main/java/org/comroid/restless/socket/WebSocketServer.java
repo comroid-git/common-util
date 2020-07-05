b25c41855f1916f00823bc27d666a3fd5bb8a450
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-07-05 15:40:11 +0200

    Update WebSocketServer.java

--- src/restless/main/java/org/comroid/restless/socket/WebSocketServer.java
+++ src/restless/main/java/org/comroid/restless/socket/WebSocketServer.java
@@ -6,6 +6,8 @@ import org.comroid.dreadpool.loop.manager.LoopManager;
 import org.comroid.listnr.AbstractEventManager;
 import org.comroid.listnr.ListnrCore;
 import org.comroid.mutatio.span.Span;
+import org.comroid.restless.REST;
+import org.comroid.restless.REST.Header.List;
 import org.comroid.restless.socket.event.WebSocketEvent;
 import org.comroid.restless.socket.event.WebSocketPayload;
 import org.comroid.uniform.SerializationAdapter;
@@ -15,10 +17,12 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.function.Consumer;
+import java.util.function.BiFunction;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 
@@ -26,7 +30,7 @@ public final class WebSocketServer extends AbstractEventManager<WebSocketData, W
     public static final FluentLogger logger = FluentLogger.forEnclosingClass();
     private final SerializationAdapter<?, ?, ?> seriLib;
     private final Executor executor;
-    private final Consumer<SocketHandler> additionalSocketHandler;
+    private final BiFunction<WebSocketServer, Socket, ? extends SocketHandler> handlerCreator;
     private final ServerSocket socket;
     private final Span<SocketHandler> handlers = new Span<>();
 
@@ -36,14 +40,13 @@ public final class WebSocketServer extends AbstractEventManager<WebSocketData, W
             InetAddress adress,
             int port
     ) throws IOException {
-        this(serializationAdapter, executor, it -> {
-        }, adress, port);
+        this(serializationAdapter, executor, SocketHandler::new, adress, port);
     }
 
     public WebSocketServer(
             SerializationAdapter<?, ?, ?> serializationAdapter,
             Executor executor,
-            Consumer<SocketHandler> additionalSocketHandler,
+            BiFunction<WebSocketServer, Socket, ? extends SocketHandler> socketHandlerCreator,
             InetAddress adress,
             int port
     ) throws IOException {
@@ -51,7 +54,7 @@ public final class WebSocketServer extends AbstractEventManager<WebSocketData, W
 
         this.seriLib = serializationAdapter;
         this.executor = executor;
-        this.additionalSocketHandler = additionalSocketHandler;
+        this.handlerCreator = socketHandlerCreator;
         this.socket = new ServerSocket(port, 0, adress);
 
         executor.execute(new SocketAcceptor());
@@ -62,7 +65,9 @@ public final class WebSocketServer extends AbstractEventManager<WebSocketData, W
     }
 
     @SuppressWarnings("FieldCanBeLocal")
-    public final class SocketHandler extends AbstractEventManager<WebSocketData, WebSocketEvent<WebSocketPayload.Data>, WebSocketPayload.Data> {
+    public static class SocketHandler
+            extends AbstractEventManager<WebSocketData, WebSocketEvent<WebSocketPayload.Data>, WebSocketPayload.Data> {
+        private final WebSocketServer socketServer;
         private final Socket socket;
         private final LoopManager loopManager;
         private final Queue<String> messageQueue = new LinkedBlockingQueue<>();
@@ -73,13 +78,15 @@ public final class WebSocketServer extends AbstractEventManager<WebSocketData, W
                         InputStreamReader isr = new InputStreamReader(socket.getInputStream());
                         BufferedReader br = new BufferedReader(isr)
                 ) {
-                    final String message = br.lines().collect(Collectors.joining());
-                    final UniNode node = seriLib.createUniNode(message);
+                    final String message = br.lines().collect(Collectors.joining("\n"));
+                    System.out.println("message = " + message);
+                    final UniNode node = socketServer.seriLib.createUniNode(message);
                     final WebSocketData data = WebSocketData.ofNode(null, node);
 
                     publish(WebSocketEvent.DATA, data);
                 } catch (IOException e) {
                     logger.at(Level.SEVERE).withCause(e).log();
+                    loopManager.close();
                 }
             }
         };
@@ -106,6 +113,7 @@ public final class WebSocketServer extends AbstractEventManager<WebSocketData, W
                         pw.println(message);
                     } catch (IOException e) {
                         logger.at(Level.SEVERE).withCause(e).log();
+                        loopManager.close();
                     }
                 }
             }
@@ -115,14 +123,44 @@ public final class WebSocketServer extends AbstractEventManager<WebSocketData, W
             return socket;
         }
 
-        public SocketHandler(Socket socket) {
-            super(WebSocketServer.this);
+        private final CompletableFuture<REST.Header.List> initialHeaders;
 
+        public CompletableFuture<List> getInitialHeaders() {
+            return initialHeaders;
+        }
+
+        public SocketHandler(WebSocketServer socketServer, Socket socket) {
+            super(socketServer);
+
+            this.socketServer = socketServer;
             this.socket = socket;
             this.loopManager = LoopManager.start(2);
 
-            loopManager.queue(reader);
-            loopManager.queue(writer);
+            this.initialHeaders = waitForHeaders()
+                    .thenApply(headers -> {
+                        loopManager.queue(reader);
+                        loopManager.queue(writer);
+                        return headers;
+                    });
+        }
+
+        private CompletableFuture<REST.Header.List> waitForHeaders() {
+            return CompletableFuture.supplyAsync(() -> {
+                try (
+                        InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+                        BufferedReader br = new BufferedReader(isr)
+                ) {
+                    final List headers = new List();
+                    br.lines()
+                            .skip(1)
+                            .map(str -> str.split(": "))
+                            .filter(box -> !box[0].isEmpty())
+                            .forEach(box -> headers.add(box[0], String.join(": ", Arrays.copyOfRange(box, 1, box.length))));
+                    return headers;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         }
 
         public void enqueue(String message) {
@@ -140,9 +178,7 @@ public final class WebSocketServer extends AbstractEventManager<WebSocketData, W
             while (true) {
                 try {
                     final Socket socket = WebSocketServer.this.socket.accept();
-                    final SocketHandler handler = new SocketHandler(socket);
-
-                    additionalSocketHandler.accept(handler);
+                    final SocketHandler handler = handlerCreator.apply(WebSocketServer.this, socket);
                     handlers.add(handler);
                 } catch (IOException e) {
                     logger.at(Level.SEVERE).withCause(e).log();
