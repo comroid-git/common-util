9f86fed7208e4ec767adf1ffa515ff9005e213e5
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-07-05 11:57:23 +0200

    HeldType extends Named

--- src/uniform/main/java/org/comroid/uniform/HeldType.java
+++ src/uniform/main/java/org/comroid/uniform/HeldType.java
@@ -1,8 +1,10 @@
 package org.comroid.uniform;
 
+import org.comroid.common.ref.Named;
+
 import java.util.function.Function;
 
-public interface HeldType<R> extends Function<String, R> {
+public interface HeldType<R> extends Function<String, R>, Named {
     <T> T convert(R value, ValueType<T> toType);
 
     R apply(String from);
--- src/uniform/main/java/org/comroid/uniform/ValueType.java
+++ src/uniform/main/java/org/comroid/uniform/ValueType.java
@@ -3,16 +3,26 @@ package org.comroid.uniform;
 import java.util.function.Function;
 
 public final class ValueType<R> implements HeldType<R> {
-    public static final ValueType<Boolean> BOOLEAN = new ValueType<>(Boolean::parseBoolean);
-    public static final ValueType<Character> CHARACTER = new ValueType<>(str -> str.toCharArray()[0]);
-    public static final ValueType<Double> DOUBLE = new ValueType<>(Double::parseDouble);
-    public static final ValueType<Float> FLOAT = new ValueType<>(Float::parseFloat);
-    public static final ValueType<Integer> INTEGER = new ValueType<>(Integer::parseInt);
-    public static final ValueType<Long> LONG = new ValueType<>(Long::parseLong);
-    public static final ValueType<Short> SHORT = new ValueType<>(Short::parseShort);
-    public static final ValueType<String> STRING = new ValueType<>(Function.identity());
-    public static final ValueType<Void> VOID = new ValueType<>(it -> null);
+    public static final ValueType<Boolean> BOOLEAN
+            = new ValueType<>("boolean", Boolean::parseBoolean);
+    public static final ValueType<Character> CHARACTER
+            = new ValueType<>("char", str -> str.toCharArray()[0]);
+    public static final ValueType<Double> DOUBLE
+            = new ValueType<>("double", Double::parseDouble);
+    public static final ValueType<Float> FLOAT
+            = new ValueType<>("float", Float::parseFloat);
+    public static final ValueType<Integer> INTEGER
+            = new ValueType<>("int", Integer::parseInt);
+    public static final ValueType<Long> LONG
+            = new ValueType<>("long", Long::parseLong);
+    public static final ValueType<Short> SHORT
+            = new ValueType<>("short", Short::parseShort);
+    public static final ValueType<String> STRING
+            = new ValueType<>("String", Function.identity());
+    public static final ValueType<Void> VOID
+            = new ValueType<>("Void", it -> null);
 
+    private final String name;
     private final Function<String, R> mapper;
 
     @Deprecated
@@ -20,7 +30,13 @@ public final class ValueType<R> implements HeldType<R> {
         return this;
     }
 
-    public ValueType(Function<String, R> mapper) {
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public ValueType(String name, Function<String, R> mapper) {
+        this.name = name;
         this.mapper = mapper;
     }
 
