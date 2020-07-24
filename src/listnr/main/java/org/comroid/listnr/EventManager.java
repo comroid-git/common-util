2dda19525f26c7f11fe07a6a32fe25672abe03a1
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-07-24 17:10:37 +0200

    Pull internal method up

--- src/listnr/main/java/org/comroid/listnr/EventManager.java
+++ src/listnr/main/java/org/comroid/listnr/EventManager.java
@@ -1,7 +1,9 @@
 package org.comroid.listnr;
 
+import org.comroid.listnr.impl.PipeAccessor;
 import org.comroid.mutatio.pipe.Pipe;
 import org.comroid.mutatio.span.Span;
+import org.jetbrains.annotations.ApiStatus.Internal;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -18,4 +20,7 @@ public interface EventManager<I extends EventPayload, T extends EventType<? supe
     <XP extends P> Pipe<?, XP> eventPipe(EventType<I, XP> type);
 
     CompletableFuture<?> publish(I payload);
+
+    @Internal
+    <XP extends P> PipeAccessor<I, XP> getPipeAccessor(EventType<I, XP> eventType);
 }
