4f656e074dc15f0511fa28d7ed95af272485d43c
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2020-08-15 13:19:39 +0200

    implemented #parse() and #typeOfData methods

--- src/uniform/main/java8compat/org/comroid/uniform/adapter/model/JacksonAdapter.java
+++ src/uniform/main/java8compat/org/comroid/uniform/adapter/model/JacksonAdapter.java
@@ -1,14 +1,18 @@
 package org.comroid.uniform.adapter.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
+import org.comroid.common.exception.AssertionException;
 import org.comroid.uniform.DataStructureType;
 import org.comroid.uniform.SerializationAdapter;
 import org.comroid.uniform.node.UniArrayNode;
 import org.comroid.uniform.node.UniNode;
 import org.comroid.uniform.node.UniObjectNode;
+import org.comroid.uniform.node.UniValueNode;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class JacksonAdapter extends SerializationAdapter<JsonNode, ObjectNode, ArrayNode> {
@@ -22,12 +26,42 @@ public abstract class JacksonAdapter extends SerializationAdapter<JsonNode, Obje
 
     @Override
     public DataStructureType<SerializationAdapter<JsonNode, ObjectNode, ArrayNode>, JsonNode, ? extends JsonNode> typeOfData(String data) {
-        throw new UnsupportedOperationException();
+        try {
+            final JsonNode node = objectMapper.readTree(data);
+
+            if (node.isArray())
+                return arrayType;
+            if (node.isObject())
+                return objectType;
+            if (node.isValueNode())
+                return null;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(String.format("Invalid %s data: \n%s", mimeType, data), e);
+        }
+
+        throw new AssertionException();
     }
 
     @Override
     public UniNode parse(@Nullable String data) {
-        throw new UnsupportedOperationException();
+        try {
+            final JsonNode node = objectMapper.readTree(data);
+
+            if (node.isArray())
+                return createUniArrayNode((ArrayNode) node);
+            if (node.isObject())
+                return createUniObjectNode((ObjectNode) node);
+            if (node.isValueNode())
+                return createValueNode((ValueNode) node);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(String.format("Invalid %s data: \n%s", mimeType, data), e);
+        }
+
+        throw new AssertionException();
+    }
+
+    private UniValueNode<String> createValueNode(ValueNode node) {
+        return
     }
 
     @Override
