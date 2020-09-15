bce5135fb3f8639e9a2d9881e7259a8b900df98c
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2020-09-15 13:55:28 +0200

    Fixed FutureReference

--- src/mutatio/main/java/org/comroid/mutatio/ref/FutureReference.java
+++ src/mutatio/main/java/org/comroid/mutatio/ref/FutureReference.java
@@ -24,9 +24,11 @@ public final class FutureReference<T> extends Reference.Support.Base<T> {
         return future.join();
     }
 
-    public T compute(Supplier<T> supplier) {
-        if (!future.isDone())
-            future.complete(supplier.get());
-        return get();
+    @Override
+    protected boolean doSet(T value) {
+        if (future.isDone())
+            return false;
+        future.complete(value);
+        return true;
     }
 }
