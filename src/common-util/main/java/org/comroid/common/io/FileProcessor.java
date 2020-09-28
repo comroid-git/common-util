84c293ef1e362d5973e7e852ea1804f3fb0725e2
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-09-28 19:17:43 +0200

    Update FileProcessor.java

--- src/common-util/main/java/org/comroid/common/io/FileProcessor.java
+++ src/common-util/main/java/org/comroid/common/io/FileProcessor.java
@@ -3,6 +3,7 @@ package org.comroid.common.io;
 import org.comroid.common.Disposable;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public interface FileProcessor extends Disposable {
     FileHandle getFile();
@@ -19,6 +20,30 @@ public interface FileProcessor extends Disposable {
             throw new RuntimeException("Could not store data", e);
         } finally {
             disposeThrow();
+        }
+    }
+
+    interface Underlying extends FileProcessor {
+        FileProcessor getUnderlyingFileProcessor();
+
+        @Override
+        default FileHandle getFile() {
+            return getUnderlyingFileProcessor().getFile();
+        }
+
+        @Override
+        default int storeData() throws IOException {
+            return getUnderlyingFileProcessor().storeData();
+        }
+
+        @Override
+        default int reloadData() throws IOException {
+            return getUnderlyingFileProcessor().reloadData();
+        }
+
+        @Override
+        default UUID getUUID() {
+            return getUnderlyingFileProcessor().getUUID();
         }
     }
 }
