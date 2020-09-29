9fd87d135c1549fbcc76e8a58bb7675a73dde4e9
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2020-09-29 16:26:19 +0200

    remove spellbind

--- src/spellbind/main/java/org/comroid/spellbind/model/TypeFragmentProvider.java
+++
@@ -1,9 +0,0 @@
-package org.comroid.spellbind.model;
-
-import org.comroid.api.Invocable;
-
-public interface TypeFragmentProvider<T extends TypeFragment<? super T>> {
-    Class<T> getInterface();
-
-    Invocable<? extends T> getInstanceSupplier();
-}
--- src/spellbind/test/java/org/comroid/test/model/FullAbstract.java
+++
@@ -1,9 +0,0 @@
-package org.comroid.test.model;
-
-import org.comroid.spellbind.model.TypeFragment;
-
-public interface FullAbstract extends TypeFragment<NonAbstract> {
-    boolean invert(boolean val);
-
-    String name();
-}
